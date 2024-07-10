8edb606000644a5939c9f9ec24cd20a1eedb3d4a
snoire <snoire@qq.com>
2024-07-10 10:07:08 +0800

    build for multiple targets to make a release

--- build.zig
+++ build.zig
@@ -1,6 +1,17 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .x86, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .riscv64, .os_tag = .linux },
+    .{ .cpu_arch = .powerpc64le, .os_tag = .linux },
+
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+};
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -8,17 +19,17 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("shell/Ast.zig"),
     });
 
-    const exe = b.addExecutable(.{
+    const shell = b.addExecutable(.{
         .name = "shell",
         .root_source_file = b.path("main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.root_module.addImport("Ast", ast_module);
+    shell.root_module.addImport("Ast", ast_module);
 
-    b.installArtifact(exe);
+    b.installArtifact(shell);
 
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(shell);
     run_cmd.step.dependOn(b.getInstallStep());
 
     // This allows the user to pass arguments to the application in the build
@@ -45,4 +56,22 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    // release
+    const release_step = b.step("release", "Build for multiple targets to make a release");
+    for (targets) |t| {
+        const exe = b.addExecutable(.{
+            .name = b.fmt("shell-{s}-{s}", .{ @tagName(t.cpu_arch.?), @tagName(t.os_tag.?) }),
+            .root_source_file = b.path("main.zig"),
+            .target = b.resolveTargetQuery(t),
+            .optimize = .ReleaseSmall,
+        });
+        exe.root_module.addImport("Ast", ast_module);
+
+        const target_output = b.addInstallArtifact(exe, .{
+            .dest_dir = .{ .override = .{ .custom = "release" } },
+        });
+
+        release_step.dependOn(&target_output.step);
+    }
 }
